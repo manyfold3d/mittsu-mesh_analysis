5acaf99e309cb594df32e58f50b3c43915ae17d5
James Smith <james@floppy.org.uk>
2024-12-03 14:37:04 +0000

    test edge loop navigation

--- spec/lib/mittsu/winged_edge_geometry_spec.rb
+++ spec/lib/mittsu/winged_edge_geometry_spec.rb
@@ -49,6 +49,37 @@ RSpec.describe Mittsu::MeshAnalysis::WingedEdgeGeometry do
         expect(edge.ccw_right).to eq 3
       end
 
+      it "has a valid cw left loop" do
+        e1 = subject.edge(edge.cw_left)
+        expect(e1.left).to eq edge.left
+        e2 = subject.edge(e1.cw_left)
+        expect(e2.left).to eq edge.left
+        expect(e2.cw_left).to eq edge.index
+      end
+
+      it "has a valid ccw left loop" do
+        e1 = subject.edge(edge.ccw_left)
+        expect(e1.left).to eq edge.left
+        e2 = subject.edge(e1.ccw_left)
+        expect(e2.left).to eq edge.left
+        expect(e2.ccw_left).to eq edge.index
+      end
+
+      it "has a valid cw right loop" do
+        e1 = subject.edge(edge.cw_right)
+        expect(e1.left).to eq edge.right
+        e2 = subject.edge(e1.cw_left)
+        expect(e2.left).to eq edge.right
+        expect(e2.cw_left).to eq edge.index
+      end
+
+      it "has a valid ccw right loop" do
+        e1 = subject.edge(edge.ccw_right)
+        expect(e1.left).to eq edge.right
+        e2 = subject.edge(e1.ccw_left)
+        expect(e2.left).to eq edge.right
+        expect(e2.ccw_left).to eq edge.index
+      end
     end
   end
 end
