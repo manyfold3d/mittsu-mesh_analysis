b517680c049ecdf02a700712c2e1a6521429450a
James Smith <james@floppy.org.uk>
2024-12-05 10:44:27 +0000

    rename example

--- examples/edge_collapse.rb
+++ examples/edge_collapse.rb
@@ -11,7 +11,7 @@ ASPECT = SCREEN_WIDTH.to_f / SCREEN_HEIGHT.to_f
 scene = Mittsu::Scene.new
 camera = Mittsu::PerspectiveCamera.new(75.0, ASPECT, 0.1, 1000.0)
 
-renderer = Mittsu::OpenGL::Renderer.new width: SCREEN_WIDTH, height: SCREEN_HEIGHT, title: 'Rendering Winged Edge Meshes'
+renderer = Mittsu::OpenGL::Renderer.new width: SCREEN_WIDTH, height: SCREEN_HEIGHT, title: 'Collapsing edges with a winged edge meshes'
 
 geometry = Mittsu::MeshAnalysis::WingedEdgeGeometry.new
 geometry.from_geometry(Mittsu::TorusGeometry.new(2, 0.8, 12, 32))
